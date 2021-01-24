6e1c125466c98ae2f609a769da8bca504713a8be
diazmx <danieldiaz3000@gmail.com>
2021-01-23 22:48:50 -0600

    Read the frecuency of a book

--- Huffman-Coding/main.py
+++ Huffman-Coding/main.py
@@ -1,4 +1,6 @@
 from huffman import *
+import string
+import re
 
 
 def readfile(f):
@@ -12,6 +14,31 @@ def readfile(f):
     return msg
 
 
+def check_frecuency_book(st):
+    # Clean text
+    st = st.lower()
+    table = str.maketrans(dict.fromkeys(string.punctuation))
+    new_s = st.translate(table)
+    table = str.maketrans(dict.fromkeys(string.digits))
+    new_s = new_s.translate(table)
+    new_s = new_s.replace("í", "i")
+    new_s = new_s.replace("ó", "o")
+    new_s = new_s.replace("á", "a")
+    new_s = new_s.replace("é", "e")
+    new_s = new_s.replace("ú", "u")
+    new_s = new_s.replace('\ufeff', "")
+    new_s = new_s.replace('«', "")
+    new_s = new_s.replace(' ', "")
+    new_s = new_s.replace('̈', "")
+    new_s = new_s.replace('̀', "")
+    new_s = new_s.replace('̃', "")
+    frec_letters = count_frecuency(new_s)
+    print(frec_letters)
+    #o_frec = open("frec_book.txt", "w")
+    for key, value in sorted(frec_letters):
+        print(key, value*100)
+
+
 def main():
     f = open("in.txt", "r")
     msg = readfile(f)
@@ -22,6 +49,11 @@ def main():
     print(test)
     test2 = decoding(test, h_codes)
     print(test2)
+
+    # PArte 2
+    fbook = open("book.txt", 'r')
+    text_book = readfile(fbook)
+    check_frecuency_book(text_book)
 
 
 if __name__ == "__main__":
